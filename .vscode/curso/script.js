55bde34859e7698cf785370a56cd0a4e966b6b00
jgoblin <kayaroki@gmail.com>
2024-06-01 15:46:18 -0300

    first commit

---
+++ .vscode/curso/script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "perfilnormal.png")
+    img.setAttribute(
+      "Alt",
+      "Uma foto de Janderson, no periodo da noite, usando blusa de frio."
+    )
+  } else {
+    img.setAttribute("src", "perfilblack.png")
+    img.setAttribute(
+      "Alt",
+      "Uma foto com efeito preto e branco de Janderson, no periodo da noite, usando blusa de frio."
+    )
+  }
+
+  /*  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+  */
+}
