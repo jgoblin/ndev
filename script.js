73c66fd9707b581289dc459d328b7f3977a51303
jgoblin <kayaroki@gmail.com>
2024-06-02 18:06:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
+
+/*
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  } */
+
   
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "/assets/assets/perfilnormal.png")
+    img.setAttribute("assets/assets/perfilnormal.png")
     img.setAttribute(
       "Alt",
       "Uma foto de Janderson, no periodo da noite, usando blusa de frio."
     )
   } else {
-    img.setAttribute("src", "/assets/assets/perfilblack.png")
+    img.setAttribute("assets/assets/perfilblack.png")
     img.setAttribute(
       "Alt",
       "Uma foto com efeito preto e branco de Janderson, no periodo da noite, usando blusa de frio."
     )
   }
-
-  /*  if (html.classList.contains("light")) {
-    html.classList.remove("light")
-  } else {
-    html.classList.add("light")
-  }
-  */
 }
